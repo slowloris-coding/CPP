051cd25c8b6cb96e5bcead060bc7a73aaa2f5f4a
slowloris-coding <65670619+slowloris-coding@users.noreply.github.com>
2022-12-07 11:26:59 +0100

    init

---
+++ projects/bruteforce/lib/bruteforce/bruteforce.cpp
@@ -0,0 +1,5 @@
+//
+// Created by loris on 07.12.2022.
+//
+
+#include "bruteforce.h"
---
+++ projects/bruteforce/lib/bruteforce/bruteforce.h
@@ -0,0 +1,8 @@
+//
+// Created by loris on 07.12.2022.
+//
+
+#ifndef BRUTEFORCE_H
+#define BRUTEFORCE_H
+
+#endif //BRUTEFORCE_BRUTEFORCE_H

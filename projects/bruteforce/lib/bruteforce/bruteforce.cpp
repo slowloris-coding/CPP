e4a3211db41d8c26584845f9607c0f240897c628
slowloris-coding <65670619+slowloris-coding@users.noreply.github.com>
2022-12-07 12:28:09 +0100

    iniz

--- projects/bruteforce/lib/bruteforce/bruteforce.cpp
+++ projects/bruteforce/lib/bruteforce/bruteforce.cpp
@@ -3,3 +3,65 @@
 //
 
 #include "bruteforce.h"
+#include <bits/stdc++.h>
+#include <regex>
+
+using namespace std;
+
+void BF::HASH::set_curr_hash_t(const string &ht_name) {
+    if(regex_match(ht_name, regex("^[Mm][Dd]\\-?5$"))){
+        BF::HASH::CURR_HASH_T = BF::HASH::HashType::md5;
+    }
+    else if(regex_match(ht_name, regex("^[Ss][Hh][Aa]\\-?1$"))){
+        BF::HASH::CURR_HASH_T = BF::HASH::HashType::sha1;
+    }
+    else if(regex_match(ht_name, regex("^[Ss][Hh][Aa]\\-?2$"))){
+        BF::HASH::CURR_HASH_T = BF::HASH::HashType::sha2;
+    }
+    else if(regex_match(ht_name, regex("^[Ss][Hh][Aa]\\-?224$"))){
+        BF::HASH::CURR_HASH_T = BF::HASH::HashType::sha224;
+    }
+    else if(regex_match(ht_name, regex("^[Ss][Hh][Aa]\\-?256$"))){
+        BF::HASH::CURR_HASH_T = BF::HASH::HashType::sha256;
+    }
+    else if(regex_match(ht_name, regex("^[Ss][Hh][Aa]\\-?348$"))){
+        BF::HASH::CURR_HASH_T = BF::HASH::HashType::sha384;
+    }
+    else if(regex_match(ht_name, regex("^[Ss][Hh][Aa]\\-?512$"))){
+        BF::HASH::CURR_HASH_T = BF::HASH::HashType::sha512;
+    }
+}
+
+string BF::HASH::hash_string(const string &secret_str) {
+    switch(BF::HASH::CURR_HASH_T){
+        case BF::HASH::HashType::md5 :
+            break;
+
+        case BF::HASH::HashType::sha1 :
+            break;
+
+        case BF::HASH::HashType::sha2 :
+            break;
+
+        case BF::HASH::HashType::sha224 :
+            break;
+
+        case BF::HASH::HashType::sha256 :
+            break;
+
+        case BF::HASH::HashType::sha384 :
+            break;
+
+        case BF::HASH::HashType::sha512 :
+            break;
+
+        default:
+            return secret_str;
+            break;
+    }
+}
+
+
+void BF::brute_force(const string &secret, void (*callback_func)(string)){
+
+}
--- projects/bruteforce/lib/bruteforce/bruteforce.h
+++ projects/bruteforce/lib/bruteforce/bruteforce.h
@@ -5,4 +5,35 @@
 #ifndef BRUTEFORCE_H
 #define BRUTEFORCE_H
 
+#include <bits/stdc++.h>
+#include <regex>
+
+using namespace std;
+
+namespace BF {
+    namespace HASH { // HSH => HASH
+        enum HashType {
+            md5 = 0 << 0,
+            sha1 = 1 << 0,
+            sha2 = 1 << 1,
+            sha256 = 1 << 2,
+            sha512 = 1 << 3,
+            sha224 = 1 << 4,
+            sha384 = 1 << 5
+        };
+
+        BF::HASH::HashType CURR_HASH_T = BF::HASH::HashType::sha1;
+
+        void set_curr_hash_t(const string &ht_name);
+        string hash_string(const string &secret_str);
+    }
+
+    namespace CS { // CS => CHAR_SET
+        const vector<char> CHAR_SET{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R','S', 'T', 'U','V','W','X', 'Y', 'Z',' ','+','"','@','*','#','%','&','/','|','(',')','=','?','\'','^','`','~','[',']','!','{','}','$','-','_','.',':',',',';','<','>','\\'};
+        const int LAST_INDEX = (BF::CS::CHAR_SET.size() - 1);
+    }
+
+    void brute_force(const string &secret, void (*callback_func)(string));
+}
+
 #endif //BRUTEFORCE_BRUTEFORCE_H
